4e0c6e5d83522fcbe6ccce9d2b0ba9f2a8e51bcf
Kenny Reida Dharmawan <dharmawankenny@gmail.com>
2019-02-11 17:51:53 +0700

    init storybook

---
+++ .storybook/config.js
@@ -0,0 +1,10 @@
+import { configure } from '@storybook/react';
+
+const req = require.context('../src', true, /.story.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+  // You can require as many stories as you need.
+}
+
+configure(loadStories, module);
